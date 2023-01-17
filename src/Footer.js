5f73d0c3e479a10cbb812e27a783812fa0c45a0a
jcarreazo <jcarreazos.dev@gmail.com>
2023-01-17 08:34:40 -0500

    Finishing Touches

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,20 @@
 import React from 'react';
-
 export default function Footer() {
   return (
-    <div>
+    <div className="Footer">
       <div className="Icons">
-        <div className="Icon-1"></div>
-        <div className="Icon-2"></div>
-        <div className="Icon-3"></div>
-        <div className="Icon-4"></div>
+        <div className="Icon-1">
+          <i class="fa-brands fa-square-twitter fa-2xl"></i>
+        </div>
+        <div className="Icon-2">
+          <i class="fa-brands fa-square-facebook fa-2xl"></i>
+        </div>
+        <div className="Icon-3">
+          <i class="fa-brands fa-square-instagram fa-2xl"></i>
+        </div>
+        <div className="Icon-4">
+          <i class="fa-brands fa-square-github fa-2xl"></i>
+        </div>
       </div>
     </div>
   );
--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,25 @@
 import React from 'react';
+const LinkedInPhoto =
+  'https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/linkedin-icon-18-256.png';
+const EmailPhoto =
+  'https://banner2.cleanpng.com/20180718/twz/kisspng-computer-icons-stock-photography-email-clip-art-simblo-5b4faed2d78991.2976490215319487548829.jpg';
 export default function Main() {
   return (
     <div className="Main">
+      <div className="ProfileButtons">
+        <div className="Email">
+          <img src={EmailPhoto} alt="" className="EmailIcon" /> Email
+        </div>
+        <div className="LinkedIn">
+          {' '}
+          <img
+            src={LinkedInPhoto}
+            alt="linkedin-ico"
+            className="LinkedInIcon"
+          />{' '}
+          LinkedIn
+        </div>
+      </div>
       <div className="About">
         <h2>About</h2>
         <p className="TextAbout">
